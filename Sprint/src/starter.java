95a080141bff41e4ed43165d32a15de773e2fecd
Diogo Miguel Madeira Pinto <88104776+TrueManOfCulture@users.noreply.github.com>
2024-11-24 19:36:00 +0000

    Add files via upload

---
+++ Sprint/src/starter.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class starter {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Main.main(new String[]{"2", "0"});
+        Main.main(new String[]{"3", "0"});
+        Main.main(new String[]{"4", "0"});
+        Main.main(new String[]{"1", "1"});
+    }
+}
